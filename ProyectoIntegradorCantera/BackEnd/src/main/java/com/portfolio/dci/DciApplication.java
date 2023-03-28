e06fbaa4eb332d0874b0fe22bd018d923b5e374f
DianaCantera <dianix0509@gmail.com>
2023-03-28 08:03:28 -0300

    BackEnd

---
+++ ProyectoIntegradorCantera/BackEnd/src/main/java/com/portfolio/dci/DciApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DciApplication.class, args);
+	}
+
+}
