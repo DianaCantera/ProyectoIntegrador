91f1f52d9fcdd2f0c968420b2af592b5eaedd25d
DianaCantera <dianix0509@gmail.com>
2023-03-28 20:31:19 -0300

    Nuevo comienzo

---
+++ ProyectoIntegradorCantera/BackEnd/src/main/java/com/portfolio/dci/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.dci.Repository;
+
+import com.portfolio.dci.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+     
+}
